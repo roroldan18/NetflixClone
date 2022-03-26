4a66b09780f473990403c4e7d03593e6d36b0537
Rodrigo Roldan <rodrigo_roldan18@hotmail.com>
2022-03-26 20:37:39 -0300

    scroll del menu de notificaciones

--- src/components/NotificationComponent.jsx
+++ src/components/NotificationComponent.jsx
@@ -28,14 +28,65 @@ export const NotificationComponent = () => {
         top10: false,
         newEpisodes: false,
         },
+        {
+        type: 'Estreno',
+        description: 'Proyecto Géminis',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/Gemini_man.webp',
+        top10: false,
+        newEpisodes: false,
+        },
+        {
+        type: 'Estreno',
+        description: 'Cangrejo Negro',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/Cangrejo-Negro.jpeg',
+        top10: true,
+        newEpisodes: true,
+        },
+        {
+        type: 'Estreno',
+        description: 'Flash',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/The-Flash.webp',
+        top10: false,
+        newEpisodes: false,
+        },
+        {
+        type: 'Estreno',
+        description: 'Proyecto Géminis',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/Gemini_man.webp',
+        top10: false,
+        newEpisodes: false,
+        },
+        {
+        type: 'Estreno',
+        description: 'Cangrejo Negro',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/Cangrejo-Negro.jpeg',
+        top10: true,
+        newEpisodes: true,
+        },
+        {
+        type: 'Estreno',
+        description: 'Flash',
+        creationDate: new Date(),
+        srcImg: '/assets/notifications/The-Flash.webp',
+        top10: false,
+        newEpisodes: false,
+        },
 
 ]
 
   return (
-    <div className='menuDropHover notificationHover'>
-        {
-            notifications.map(notification => <SingleNotification notification={notification} /> )
-        }
-    </div>
+    <div className='menuDropHoverNotification'>
+      <i className="fa-solid fa-caret-up m-0 arrowNotificacion"/>
+      <div className='contentNotification '>
+            {
+                notifications.map(notification => <SingleNotification notification={notification} /> )
+            }
+        </div>
+      </div>
   )
 }
