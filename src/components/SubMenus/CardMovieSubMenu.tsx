a5607073dbe5c661a8b5e0d646c50a26fe43b37e
Rodrigo Roldan <rodrigo_roldan18@hotmail.com>
2022-04-26 15:50:37 -0300

    try 1 hover on Menu

--- src/components/SubMenus/CardMovieSubMenu.tsx
+++ src/components/SubMenus/CardMovieSubMenu.tsx
@@ -1,36 +1,50 @@
-import React, { useState } from 'react'
+import React from 'react';
 import './SubMenus.scss';
+import './CardMovie.scss';
 import { PropCardMoreInfo } from '../../utils/types';
 
 export const CardMovieSubMenu = ({option}:PropCardMoreInfo) => {
-
-  const [isHovering, setIsHovering] = useState(false);
-
-  let timer: number|null = null;
-  const TIMEOUT = 1000;
-
-  const handleMouseOver = () => {
-    timer = window.setTimeout(() => {
-      console.log(`mouse Over ${option.order}`);
-      setIsHovering(true);
-    }, TIMEOUT);
+      return (
+        <div className='imgMovieCardDiv'>
+          <img className="card-img-top imgMovieCard" src={option.src} alt={option.alt} />
+          <div className='infoCard'>
+            <div className='iconFirstLine d-flex'>
+              <div className='containerIcon playIcon'>
+                <i className="fa-solid fa-play "></i>
+              </div>
+              <div className='containerIcon normalIcon'>
+                <i className="fa-solid fa-plus "></i>
+              </div>
+              <div className='containerIcon normalIcon'>
+              <i className="fa-solid fa-thumbs-up"></i>
+              </div>
+              <div className='containerIcon normalIcon'>
+                <i className="fa-solid fa-thumbs-down"></i>
+              </div>
+              <div className='containerIcon lastIcon'>
+                <i className="fa-solid fa-angle-down"></i>
+              </div>
+            </div>
+            <div className='secondLine d-flex'>
+              <h4>99% para ti</h4>
+              <p>16+</p>
+              <h5>6 temporadas</h5>
+              <p>HD</p>
+            </div>
+            <div className='thirdLine'>
+              <p>Inusual</p>
+              <span className="separatorCategory"></span>
+              <p>Drama</p>
+              <span className="separatorCategory"></span>
+              <p>Crudo</p>
+            </div>
+            <div className='forthLine'>
+              <div className='likeNetflix'>
+                <i className="fa-solid fa-thumbs-up"></i>
+              </div>
+              <p>Con más «Me gusta»</p>
+            </div>
+          </div>
+        </div>
+      );
   }
-
-  const handleMouseOut = () => {
-    clearTimeout(timer as number);
-    timer = null;
-    setIsHovering(false);
-    console.log(`mouse out ${option.order}`);
-  }
-
-  
-  if(isHovering){
-    return (
-        <img className="card-img-top" src={option.src} alt={option.alt} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>
-      )
-  } else{
-    return (
-        <img className="card-img-top imgMovieCard" src={option.src} alt={option.alt} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>
-      )
-  }
-}
